371a5a9d873b4d405a6c584a6572fa16dd5d4221
shilongtang <shilong.tang@grandinsight.com>
2018-05-02 23:37:31 +0800

    sql 服务

--- service/SqlCompute/src/main/java/com/example/sqlcompute/controller/SqlComputeController.java
+++ service/SqlCompute/src/main/java/com/example/sqlcompute/controller/SqlComputeController.java
@@ -1,20 +1,27 @@
 package com.example.sqlcompute.controller;
 
 import com.example.sqlcompute.service.SqlComputeService;
+import com.example.sqlcompute.utils.ApiResponse;
 import com.example.sqlcompute.utils.BaseController;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Api(description="sql  服务", tags = "SQL API")
+@ConditionalOnWebApplication
+@RestController
+@RequestMapping("/api")
 public class SqlComputeController extends BaseController {
 
     @Autowired
-    private SqlComputeService sqlComputeSvcService;
+    private SqlComputeService sqlComputeService;
 
     /**
      * 查询sql带分页
@@ -23,11 +30,11 @@ public class SqlComputeController extends BaseController {
     @ApiOperation(value="定制接口: 查询sql带分页")
     public ApiResponse querySqlByPage(@RequestBody String body) {
         try {
-            return super.callbackSuccess(sqlComputeSvcService.querySqlByPage(body));
+            String s = sqlComputeService.querySqlByPage(body);
+            return super.callbackSuccess(sqlComputeService.querySqlByPage(body));
         }catch (Exception e){
             String _message = "查询sql带分页失败: "+e.getMessage();
             logger.error(_message, e);
-            printStackTrace(e);
             Map<String, Object> map = new HashMap<>();
             map.put("message", _message);
             return super.callbackFail(map);
@@ -40,7 +47,7 @@ public class SqlComputeController extends BaseController {
     @ApiOperation(value="定制接口: 查询sql不带分页")
     public ApiResponse querySql(@RequestBody String body) {
         try {
-            return super.callbackSuccess(sqlComputeSvcService.querySql(body));
+            return super.callbackSuccess(sqlComputeService.querySql(body));
         }catch (Exception e){
             String _message = "查询sql不带分页失败: "+e.getMessage();
             logger.error(_message, e);
@@ -73,7 +80,7 @@ public class SqlComputeController extends BaseController {
     @ApiOperation(value="定制接口: 导出excel文档")
     public ApiResponse exportExcel(@RequestBody String body) {
         try {
-            return super.callbackSuccess(sqlComputeSvcService.exportExcel(body));
+            return super.callbackSuccess(sqlComputeService.exportExcel(body));
         }catch (Exception e){
             String _message = "导出excel文档失败: "+e.getMessage();
             logger.error(_message, e);
